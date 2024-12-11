61e9d451d038b4397ee23fe40bab54e193605fff
prwork8101999 <prwork8101999@gmail.com>
2024-12-11 14:13:49 +0530

    Basic php app

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// A simple PHP application for Elastic Beanstalk deployment
+
+// Display a welcome message
+echo "<h1>Welcome to XYZ Corporation's PHP Application</h1>";
+echo "<p>This is a basic application deployed using AWS Elastic Beanstalk.</p>";
+?>
